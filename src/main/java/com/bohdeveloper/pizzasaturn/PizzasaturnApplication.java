da419ddfea7e0fe89dd64f8039ef2bab03d850f1
bohdeveloper <ohb.seven@gmail.com>
2021-08-29 10:47:52 +0200

    Primer commit

---
+++ src/main/java/com/bohdeveloper/pizzasaturn/PizzasaturnApplication.java
@@ -0,0 +1,13 @@
+package com.bohdeveloper.pizzasaturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzasaturnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzasaturnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bohdeveloper/pizzasaturn/PizzasaturnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohdeveloper.pizzasaturn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzasaturnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
