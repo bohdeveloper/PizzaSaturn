c59f89c668d2f0173d8096c00bdeb0b8a96383d2
bohdeveloper <ohb.seven@gmail.com>
2021-08-31 22:47:56 +0200

    Index

---
+++ src/main/java/com/bohdeveloper/pizzasaturn/controladores/NavegationController.java
@@ -0,0 +1,14 @@
+package com.bohdeveloper.pizzasaturn.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NavegationController {
+    
+    @GetMapping("/")
+    public String goIndex(){
+        return "index";
+    }
+
+}
